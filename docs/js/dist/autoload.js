50434b8f74c625f1c990d8d24d2c56237f99d212
nomdn <2529897207@qq.com>
2025-08-09 20:52:30 +0800

    0.10.0

--- docs/js/dist/autoload.js
+++ docs/js/dist/autoload.js
@@ -5,7 +5,7 @@
 
 // Recommended to use absolute path for live2d_path parameter
 // live2d_path 参数建议使用绝对路径
-const live2d_path = './js/dist/';
+const live2d_path = '/js/dist/';
 // const live2d_path = '/dist/';
 
 // Method to encapsulate asynchronous resource loading
